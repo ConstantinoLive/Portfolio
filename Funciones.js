82200284df1f444c429982323b120fa5db63233c
Constantino <116948630+ConstantinoLive@users.noreply.github.comse>
2024-02-23 16:47:34 -0300

    se agrega la primera version de la pagina portfolio y division de archivos css por pagina

--- Funciones.js
+++ Funciones.js
@@ -55,23 +55,6 @@ const distanceBetween = (p1x, p1y, p2x, p2y) => {
 
 /************************Trabajo****************************/
 
-/*
-const buttons = document.querySelectorAll(".card-buttons button");
-const sections = document.querySelectorAll(".card-section");
-const card = document.querySelector(".card");
-const handleButtonClick = e => {
-    const targetSection = e.target.getAttribute("data-section");
-    const section = document.querySelector(targetSection);
-    targetSection !== "#about" ? card.classList.add("is-active") : card.classList.remove("is-active");
-    card.setAttribute("data-state", targetSection);
-    sections.forEach(s => s.classList.remove("is-active"));
-    buttons.forEach(b => b.classList.remove("is-active"));
-    e.target.classList.add("is-active");
-    section.classList.add("is-active");
-};
-buttons.forEach(btn => {
-    btn.addEventListener("click", handleButtonClick);
-});*/
 
 /*
 FUNCION PARA GIRAR EL CARRUSEL CON EL MOUSE
