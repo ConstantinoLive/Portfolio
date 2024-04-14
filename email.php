f252a5780d3db9b8f1d3ba10acb8f771a70b97e5
Constantino <116948630+ConstantinoLive@users.noreply.github.comse>
2024-04-14 11:49:58 -0300

    Modificación al formulario

--- email.php
+++ email.php
@@ -1,17 +1,28 @@
 <?php
 $name = $_POST['firstname'];
-$name = $_POST['lastname'];
+$lastname = $_POST['lastname'];
 $phone = $_POST['telefono'];
 $mail = $_POST['email'];
 $asunto = $_POST['subject'];
 $message = $ POST['comentarios'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+if(empty(trim($name)))
+{
+    $name='Anonimo';
+}
+if(empty(trim($lastname)))
+{
+    $name=' ';
+}
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+//$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain; 
+charset=uft-8 \r\n";
+$header .= "From: $name $lastname <$mail> \r\n";
+$header .= "To: fernandoclingo@live.com.ar \r\n"
+
+$message .= "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su teléfono es: " . $phone . "\r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Asunto: " . $asunto . " \r\n";
