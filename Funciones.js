247cf52b5dd48b13e215d5feaeafb8c7b196f8a2
Constantino <116948630+ConstantinoLive@users.noreply.github.comse>
2025-08-30 11:41:35 -0300

    Actualizacion de CV. Se agregan los proyectos Tecno Hogar y Try-n-save al portfolio

---
+++ Funciones.js
@@ -0,0 +1,108 @@
+/*******************Contacto*************************** */
+
+const button = document.querySelector('#botonMovable');
+
+function areFieldsIncomplete() {
+    const formulario = document.getElementById('Formulario');
+    const camposInputs = formulario.querySelectorAll('input');
+    const camposInput_text = formulario.querySelectorAll('textaera');
+
+    for (const input of camposInputs) {
+        if (input.value === '') {
+            return true;
+        }
+    }
+
+    for (const textaera of camposInput_text) {
+        if (textaera.value === '') {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+document.addEventListener('mousemove', (event) => {
+    if (areFieldsIncomplete()) {
+        const radius = Math.max(
+            button.offsetWidth * 0.75,
+            button.offsetHeight * 0.75,
+            100
+        );
+        const wrapper = button.parentNode;
+        const bx = wrapper.offsetLeft + button.offsetLeft + button.offsetWidth / 2;
+        const by = wrapper.offsetTop + button.offsetTop + button.offsetHeight / 2;
+        const { clientX, clientY } = event;
+        const dist = distanceBetween(clientX, clientY, bx, by);
+        const angle = Math.atan2(clientY - by, clientX - bx);
+        const ox = -1 * Math.cos(angle) * Math.max(radius - dist, 0);
+        const oy = -1 * Math.sin(angle) * Math.max(radius - dist, 0);
+        const rx = oy / 2;
+        const ry = -ox / 2;
+
+        button.style.transform = `translate(${ox}px, ${oy}px) rotateX(${rx}deg) rotateY(${ry}deg)`;
+        button.style.boxShadow = `0px ${Math.abs(oy)}px ${Math.abs(oy) / radius * 40} rgba(0,0,0,0.15)`;
+    }
+});
+
+const distanceBetween = (p1x, p1y, p2x, p2y) => {
+    const dx = p1x - p2x;
+    const dy = p1y - p2y;
+    return Math.sqrt(dx * dx + dy * dy);
+};
+
+/************************Trabajo****************************/
+
+/*
+FUNCION PARA GIRAR EL CARRUSEL CON EL MOUSE
+
+var angle=0;
+function galleryspin(sing){
+spinner=document.querySelector('#spinner');
+if(!sing){
+    angle=angle+45;
+}
+else {
+angle=angle-45;
+}
+spinner.setAttribute("style","-webkit-transform:rotateY("+angle+"deg)")
+}
+*/
+
+/*FUNCION PARA QUE GIRE SOLO EL CARRUSEL*/
+
+var angle = 0;
+var rotationInterval;
+
+function startRotation() {
+    rotationInterval = setInterval(function () {
+        angle -= 45;
+        rotateCarousel();
+    }, 1500); // velocidad de rotación
+}
+
+function stopRotation() {
+    clearInterval(rotationInterval);
+}
+
+function rotateCarousel() {
+    var spinner = document.querySelector('#spinner');
+    spinner.style.transform = 'rotateY(' + angle + 'deg)';
+}
+
+// Iniciar la rotación automáticamente al cargar la página
+document.addEventListener('DOMContentLoaded', function () {
+    startRotation();
+});
+
+// Detener la rotación cuando el mouse entra en el área del carrusel
+document.querySelector('#carousel').addEventListener('mouseenter', function () {
+    stopRotation();
+});
+
+// Reiniciar la rotación cuando el mouse sale del área del carrusel
+document.querySelector('#carousel').addEventListener('mouseleave', function () {
+    startRotation();
+});
+
+
